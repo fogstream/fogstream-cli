bbfb676b8e2829fe717ff0ed0b1b4f0f8c6c58b3
Dmitry Kotov <dmitrii.kotov@fogstream.ru>
2019-01-25 13:28:33 +1000

    Documentation

---
+++ README.md
@@ -0,0 +1,23 @@
+FOGSTREAM CLI
+======================
+
+INSTALLATION
+--------
+
+Command line interface for easy work on projects of the company FOGSTREAM
+
+In force of using a python3 there is strongly recommended install cli in global environment 
+only via pip3.
+Or use pip if your environment based on python3.
+
+COMMANDS
+--------
+### Initiate project
+```commandline
+Usage: fogstream-cli initproject [OPTIONS] PROJECT_NAME GIT_URL
+
+Options:
+  -d        Path to project directory
+  --help    Show this message and exit.
+
+```
--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 import setuptools
 
 setuptools.setup(
-    name="fogstream_cli",
+    name="fogstream-cli",
     version="0.0.1",
     author="Dmitry Kotov",
     author_email="dmitrii.kotov@fogstream.com",
     description="Fogstream CLI",
     long_description="Fogstream CLI",
     packages=setuptools.find_packages(),
+    url='https://github.com/fogstream/fogstream-cli',
     entry_points={
         'console_scripts': ['fogstream-cli=fogstream_cli.cli:main'],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Linux",
+        "Operating System :: POSIX",
     ],
     install_required=[
         'GitPython==2.1.11',
